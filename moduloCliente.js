85a18ebebb9df9edc0466a6c770be9d8936c7b39
amauriaureo <amauriaureo@gmail.com>
2021-10-03 20:54:50 -0300

    importando modulo A

--- moduloCliente.js
+++ moduloCliente.js
@@ -1,1 +1,9 @@
 const moduloA = require('./moduloA')
+const moduloB = require('./moduloB')
+
+console.log(moduloA.ola)
+console.log(moduloA.bienVenido)
+console.log(moduloA.hastaLuego)
+console.log(moduloA)
+
+
